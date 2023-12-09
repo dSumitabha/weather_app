3edf23e14f5f6a19092dd9601525c0c49381db3b
dSumitabha <150615915+dSumitabha@users.noreply.github.com>
2023-12-09 23:00:55 +0530

    added clock features

--- script.js
+++ script.js
@@ -3,26 +3,10 @@ const main = document.querySelector('main')
 let searchButton = document.querySelector('.search-button')
 searchButton.addEventListener('click', getWeather)
 let response
-function getLocalTime(timestamp, timezone) {
-	let dateTime = new Date(timestamp * 1000)
-	dateTime = dateTime.getSeconds() + timezone;
-	const hours = dateTime.getHours();
-	const minutes = dateTime.getMinutes();
-	return hours + ':' + minutes
-	//1700789083 19800
-}
 function getWeather(){
 	event.preventDefault()
 	let queryCity = document.querySelector('.query-city').value
-	const statusVisusal = document.querySelector('.status-visual')
-	const stat = document.querySelector('.stat')
-	const cityName = document.querySelector('.city-name')
-	const temp = document.querySelector('#temp')
-	const humidity = document.querySelector('.humidity')
-	const airPressure = document.querySelector('.air-pressure')
-	const visibility = document.querySelector('.visibility')
-	const windSpeed = document.querySelector('.wind-speed')
-	const windDir = document.querySelector('.wind-direction')
+	let dynamicOffset
 	const url = 'https://api.openweathermap.org/data/2.5/weather?appid=01274dbc7b3f1ca72f51fcbe017f290b&q='+queryCity
 	const xhttpr = new XMLHttpRequest()
 	xhttpr.open('GET', url , true)
@@ -32,21 +16,16 @@ function getWeather(){
 			if(xhttpr.status === 200){
 				response = JSON.parse(xhttpr.response)
 				console.log(response)
-				statusVisusal.src = ` https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
-				stat.innerText = response.weather[0].description
-				cityName.innerText = response.name
-				temp.innerText = Math.round(response.main.temp - 273.15)
-				humidity.innerText = response.main.humidity + ' %'
-				airPressure.innerText = response.main.pressure + ' hPa'
-				visibility.innerText = response.visibility + ' m'
-				windSpeed.innerText = Math.round(response.wind.speed * 3.6) + ' kmph'
-				windDir.style.transform = `rotate(${response.wind.deg}deg)`
+				populateBody()
+				setClock()
+				populteSunset()
+				populteSunrise()
 				main.style.display = "flex"
 				firstPage.remove()
 				searchButton = document.querySelector('.search-button')
 				searchButton.addEventListener('click', getWeather)
-			//	console.log(getLocalTime(response.sys.sunrise, response.timezone))
 				queryCity = ''
+				console.log(dynamicOffset)
 			}
 			else {
 				showError()
@@ -70,3 +49,58 @@ function showError() {
 	
 }
 
+function showEff(items) {
+	items.forEach(anim)
+	function anim(item, index){
+		setTimeout(() => {
+            item.style.opacity = '1'
+        }, index * 100)
+	}
+}
+function populateBody() {
+	const statusVisusal = document.querySelector('.status-visual')
+	const stat = document.querySelector('.stat')
+	const cityName = document.querySelector('.city-name')
+	const temp = document.querySelector('#temp')
+	const humidity = document.querySelector('.humidity')
+	const airPressure = document.querySelector('.air-pressure')
+	const visibility = document.querySelector('.visibility')
+	const windSpeed = document.querySelector('.wind-speed')
+	const windDir = document.querySelector('.wind-direction')
+	const statusCards = document.querySelectorAll('.status-card')
+	statusVisusal.src = ` https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+	stat.innerText = response.weather[0].description
+	cityName.innerText = response.name
+	temp.innerText = Math.round(response.main.temp - 273.15)
+	humidity.innerText = response.main.humidity + ' %'
+	airPressure.innerText = response.main.pressure + ' hPa'
+	visibility.innerText = response.visibility + ' m'
+	windSpeed.innerText = Math.round(response.wind.speed * 3.6) + ' kmph'
+	windDir.style.transform = `rotate(${response.wind.deg}deg)`
+}
+function targetElements(){
+
+}
+
+function setClock(){
+	const digiClock = document.querySelector('.digi-clock')
+	let clock = new Date()
+	dynamicOffset = ((response.timezone + (clock.getTimezoneOffset()) * 60) * 60)
+	console.log(dynamicOffset)
+	clock = new Date(clock.getTime() + dynamicOffset)
+	clock = `${clock.getHours().toString()}:${clock.getMinutes().toString()}`
+	digiClock.innerText = clock
+}
+function populteSunrise(){
+	const sunriseTime = document.querySelector('.sunrise-time')
+	let clock = new Date(response.sys.sunrise*1000 + dynamicOffset)
+	clock = `${clock.getHours().toString()}:${clock.getMinutes().toString()}`
+	sunriseTime.innerText = clock;
+}
+
+function populteSunset(){
+	const sunsetTime = document.querySelector('.sunset-time')
+	let clock = new Date(response.sys.sunset*1000 + dynamicOffset)
+	clock = `${clock.getHours().toString()}:${clock.getMinutes().toString()}`
+	sunsetTime.innerText = clock;
+}
